690581525023758dd7eb13c4916f2ed3439259bf
Ansh gala <ansh.gala@TruBoardPartners.com>
2025-09-08 10:43:39 +0530

    Handled CORS

--- src/main/java/com/Truboard/ExcelFileDetector/controller/ExcelUploadController.java
+++ src/main/java/com/Truboard/ExcelFileDetector/controller/ExcelUploadController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", 
+                        "http://localhost:3000",
+                        "http://192.168.10.166:3000"})
 @RequestMapping("/api/excel")
 public class ExcelUploadController {
 
